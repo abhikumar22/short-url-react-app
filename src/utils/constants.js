bb8af73af4623e3b96856b5942b9400f0c848cd5
abhikumar22 <abhi.ckp1002@gmail.com>
2020-03-22 22:23:33 +0530

    change base url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://127.0.0.1:8000/"
+export const BASE_URL = "https://url1short.herokuapp.com/"
 export const GET_SHORT_URL = "GetShortUrl"
 export const GET_FULL_URL = "GetFullUrl"

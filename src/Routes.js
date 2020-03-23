b31514b93789881fc775a6a4bf73e7d8e7fa88ea
abhikumar22 <abhi.ckp1002@gmail.com>
2020-03-23 11:23:52 +0530

    checked

--- src/Routes.js
+++ src/Routes.js
@@ -2,14 +2,16 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Contact from "./Contact/Contact";
 import Home from "./Home/Home";
+import history from './history';
+
 
 export default class Routes extends Component {
     render() {
         return (
-            <Router>
+            <Router history={history}>
                 <Switch>
                     <Route exact path="/" component={Home} />
-                    {/* <Route path="*" component={Contact} /> */}
+                    <Route path="*" component={Contact} />
                 </Switch>
             </Router>
         )
